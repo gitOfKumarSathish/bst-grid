cdcc42350f8c746ecf644a57bbe963b08e49f93e
gitOfKumarSathish <bhadri2002@gmail.com>
2026-08-26 11:54:30 +0000

    chore: remove old CI and deployment workflows; add new CI workflow for docs

--- apps/docs/src/pages/index.js
+++ apps/docs/src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Redirect } from '@docusaurus/router'
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
 
 // The site is docs-only; send / straight to the Getting Started page.
 export default function Home() {
-  return <Redirect to="/docs/getting-started" />
+  return <Redirect to="/bst-grid/docs/getting-started" />;
 }
